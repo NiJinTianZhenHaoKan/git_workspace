d712fba8ef2ea16e23e3e5d9e78f0d0360ea9fd2
SMJ <503869311@qq.com>
2020-06-03 16:03:34 +0800

    add file

---
+++ HelloGit/src/main/java/com/Git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.Git;
+
+/**
+ * @author SMJ
+ * @create 2020-06-03 15:54
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
